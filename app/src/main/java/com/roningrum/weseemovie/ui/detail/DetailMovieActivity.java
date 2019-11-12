db4394a32edc45be1f42e607c4534c9055b2b671
Rizki Oktavia Ningrum <rizkioktavianingrum@gmail.com>
2019-11-12 08:04:36 +0700

    Update Project

---
+++ app/src/main/java/com/roningrum/weseemovie/ui/detail/DetailMovieActivity.java
@@ -0,0 +1,16 @@
+package com.roningrum.weseemovie.ui.detail;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.roningrum.weseemovie.R;
+
+public class DetailMovieActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail_movie);
+    }
+}
