d110608b57cfd39964bda258b65509ad31628e50
Rizki Oktavia Ningrum <rizkioktavianingrum@gmail.com>
2019-11-12 10:45:27 +0700

    Update Project

---
+++ app/src/main/java/com/roningrum/weseemovie/utils/MyAppGlideModule.java
@@ -0,0 +1,8 @@
+package com.roningrum.weseemovie.utils;
+
+import com.bumptech.glide.annotation.GlideModule;
+import com.bumptech.glide.module.AppGlideModule;
+
+@GlideModule
+public final class MyAppGlideModule extends AppGlideModule {
+}
