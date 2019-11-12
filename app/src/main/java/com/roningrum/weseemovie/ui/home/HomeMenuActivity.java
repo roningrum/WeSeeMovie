ee607bb6894e041186fbb1da12feef7321e5042e
Rizki Oktavia Ningrum <rizkioktavianingrum@gmail.com>
2019-11-12 11:28:32 +0700

    Update Project

--- app/src/main/java/com/roningrum/weseemovie/ui/home/HomeMenuActivity.java
+++ app/src/main/java/com/roningrum/weseemovie/ui/home/HomeMenuActivity.java
@@ -1,19 +1,35 @@
 package com.roningrum.weseemovie.ui.home;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.roningrum.weseemovie.R;
 
 public class HomeMenuActivity extends AppCompatActivity {
+    private BottomNavigationView.OnNavigationItemSelectedListener onNavigationItemSelectedListener = new BottomNavigationView.OnNavigationItemSelectedListener() {
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+            switch (menuItem.getItemId()) {
+                case R.id.nav_movie_menu:
+                    return true;
+                case R.id.nav_tv_menu:
+                    return true;
+
+            }
+            return false;
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_menu);
         BottomNavigationView navView = findViewById(R.id.nav_view);
+        navView.setOnNavigationItemSelectedListener(onNavigationItemSelectedListener);
     }
 
 }
