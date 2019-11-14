4e94413599f074524fbfaa8a20f5e74f0cf9497d
Rizki Oktavia Ningrum <rizkioktavianingrum@gmail.com>
2019-11-14 12:25:46 +0700

    Update Instrumentation Testing

---
+++ app/src/androidTest/java/com/roningrum/weseemovie/ui/movie/MovieListFragmentTest.java
@@ -0,0 +1,34 @@
+package com.roningrum.weseemovie.ui.movie;
+
+import androidx.test.rule.ActivityTestRule;
+
+import com.roningrum.weseemovie.R;
+import com.roningrum.weseemovie.testing.SingleFragmentActivity;
+import com.roningrum.weseemovie.utils.RecyclerViewItemCountAssertion;
+
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+
+
+public class MovieListFragmentTest {
+    @Rule
+    public ActivityTestRule<SingleFragmentActivity> activityRule = new ActivityTestRule<>(SingleFragmentActivity.class);
+    private MovieListFragment movieListFragment = new MovieListFragment();
+
+    @Before
+    public void setUp() {
+        activityRule.getActivity().setFragment(movieListFragment);
+    }
+
+    @Test
+    public void loadMovies() {
+        onView(withId(R.id.rv_movie)).check(matches(isDisplayed()));
+        onView(withId(R.id.rv_movie)).check(new RecyclerViewItemCountAssertion(11));
+    }
+}
