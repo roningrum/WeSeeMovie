30f24bc8121ef2f834bb336fe00bda65da4f74c5
Rizki Oktavia Ningrum <rizkioktavianingrum@gmail.com>
2019-11-14 21:49:58 +0700

    Update Unit Testing

---
+++ app/src/androidTest/java/com/roningrum/weseemovie/ui/movie/MovieTest.java
@@ -0,0 +1,31 @@
+package com.roningrum.weseemovie.ui.movie;
+
+import androidx.test.espresso.contrib.RecyclerViewActions;
+import androidx.test.rule.ActivityTestRule;
+
+import com.roningrum.weseemovie.R;
+import com.roningrum.weseemovie.ui.home.HomeMenuActivity;
+
+import org.junit.Rule;
+import org.junit.Test;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+
+public class MovieTest {
+    @Rule
+    public ActivityTestRule<HomeMenuActivity> activityTestRule = new ActivityTestRule<>(HomeMenuActivity.class);
+
+    @Test
+    public void toDetailMovieActivity() {
+        onView(withId(R.id.rv_movie)).check(matches(isDisplayed()));
+        onView(withId(R.id.rv_movie)).perform(RecyclerViewActions.actionOnItemAtPosition(1, click()));
+        onView(withId(R.id.tv_name_movie_detail)).check(matches(isDisplayed()));
+        onView(withId(R.id.tv_name_movie_detail)).check(matches(withText("Alita: Battle Angel(2019)")));
+    }
+
+}
