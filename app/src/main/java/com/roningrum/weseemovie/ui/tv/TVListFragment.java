6be35c08b7a439ce744150eb34b3d480f4cd2a55
Rizki Oktavia Ningrum <rizkioktavianingrum@gmail.com>
2019-11-15 21:56:13 +0700

    Update Project

--- app/src/main/java/com/roningrum/weseemovie/ui/tv/TVListFragment.java
+++ app/src/main/java/com/roningrum/weseemovie/ui/tv/TVListFragment.java
@@ -6,19 +6,34 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProviders;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.roningrum.weseemovie.R;
+import com.roningrum.weseemovie.data.TVShow;
+
+import java.util.List;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class TVListFragment extends Fragment {
+    private RecyclerView rvTVshows;
 
 
     public TVListFragment() {
         // Required empty public constructor
+    }
+
+    public static Fragment newInstances() {
+        return new TVListFragment();
     }
 
     public static Fragment newInstance() {
@@ -33,4 +48,29 @@ public class TVListFragment extends Fragment {
         return inflater.inflate(R.layout.fragment_tvlist, container, false);
     }
 
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        rvTVshows = view.findViewById(R.id.rv_tv_show);
+        Toolbar toolbar = view.findViewById(R.id.toolbar);
+        toolbar.setTitle(R.string.tv_series);
+        toolbar.setTitleTextColor(getResources().getColor(android.R.color.white));
+        ((AppCompatActivity) getActivity()).setSupportActionBar(toolbar);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        if (getActivity() != null) {
+            TVShowViewModel tvShowViewModel = ViewModelProviders.of(this).get(TVShowViewModel.class);
+            List<TVShow> tvShows = tvShowViewModel.getAllTvShows();
+
+            TVShowAdapter tvShowAdapter = new TVShowAdapter(getActivity());
+            tvShowAdapter.setTvShows(tvShows);
+
+            rvTVshows.setLayoutManager(new LinearLayoutManager(getActivity()));
+            rvTVshows.setHasFixedSize(true);
+            rvTVshows.setAdapter(tvShowAdapter);
+        }
+    }
 }
